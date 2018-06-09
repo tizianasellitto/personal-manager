092e212dcaf0a5e6fcaee6c007b210b70a0bb143
Tiziana Sellitto <tiziana.sel@gmail.com>
2018-06-09 11:41:20 -0700

    Initial Spring Boot Repository

---
+++ src/main/java/com/tizianasellitto/PersonalManagerApplication.java
@@ -0,0 +1,12 @@
+package com.tizianasellitto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalManagerApplication.class, args);
+	}
+}
